3200d74cf56cf33ad278a2cd1cefbb658d557f35
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-13 16:48:38 +0000

    13.11.18 @ 16:50 working on html hiding and showing elements

--- js/interface.js
+++ js/interface.js
@@ -2,11 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log( "ready!" );
 });
 
-function myFunction() {
+function showFullNote() {
     var x = document.getElementById('note-add-list');
     if (x.style.display === 'none') {
         x.style.display = 'block';
-    } else {
+    }
+    else {
         x.style.display = 'none';
     }
+}
+
+function homeButton() {
+  var x = document.getElementById('users-note');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  }
+  else {
+      x.style.display = 'none';
+  }
 }
