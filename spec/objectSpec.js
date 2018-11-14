b441bc0f95f816e049a361c11d83c9bee21d23ea
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-14 12:54:02 +0000

    refactored passing tests

--- spec/objectSpec.js
+++ spec/objectSpec.js
@@ -1,14 +1,13 @@
 var note1 = noteMaker("This is a message for testing")
 
+var note2 = noteMaker("This is a message for testing")
+
  describe('Note', function (){
    it('creates a note', function() {
      match(note1.getNote(), "This is a message for testing");
    });
- });
 
- var note2 = noteMaker("This is a message for testing")
 
- describe('Note', function (){
   it('has a description', function() {
     match(note2.getNoteForDisplay(), "This is a message fo")
   });
