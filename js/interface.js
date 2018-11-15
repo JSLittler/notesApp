17730d8ec9e61e868b325676e8559a896726d3ce
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-15 12:16:31 +0000

    functioning note display

--- js/interface.js
+++ js/interface.js
@@ -1,21 +1,32 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log( "ready!" );
+    var msgIndex = 0
     var button = document.getElementById("note_submit")
-      button.addEventListener("click", function () {
+    button.addEventListener("click", function () {
       var mesg = add_note.value
       var textInput = noteMaker(mesg)
       var listItem = document.createElement("li")
       var linkItem = document.createElement("a")
       var parent = document.getElementById('list_links')
+      linkItem.addEventListener("click", function () {
+        var output = document.getElementById("note")
+        output.textContent = (notesArray[this.id])
+      })
+      linkItem.setAttribute("class", "view_full_note")
+      linkItem.setAttribute("id", msgIndex)
       linkItem.setAttribute("href", "javascript:SwapDivsWithClick('note_add','users_note')")
       linkItem.textContent = textInput.getNoteForDisplay();
-        createTags(parent, listItem, linkItem)
-        add_note.value = "";
-      });
+      nestTags(parent, listItem, linkItem)
+
+      msgIndex ++;
+      console.log(linkItem)
+      add_note.value = "";
     });
 
-function createTags(a, b, c) {
+  });
+
+function nestTags(a, b, c) {
   var nodes = arguments;
     for(var i = 1; i < nodes.length; i++) {
       nodes[i - 1].appendChild(nodes[i]);
@@ -23,20 +34,15 @@ function createTags(a, b, c) {
   }
 
 
-function SwapDivsWithClick(div1,div2)
 
-{
+function SwapDivsWithClick(div1,div2) {
    d1 = document.getElementById(div1);
    d2 = document.getElementById(div2);
-   if( d2.style.display == "none" )
-   {
+   if( d2.style.display == "none" ) {
       d1.style.display = "none";
       d2.style.display = "block";
-   }
-   else
-   {
+   } else {
       d1.style.display = "block";
       d2.style.display = "none";
    }
-
 }
